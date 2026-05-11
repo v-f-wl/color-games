2812db5ebd887f705acd3508acf87a94401ea544
Kim Valentin <co.unicorno@mail.ru>
2026-05-11 22:13:03 +0300

    feat: add scoring system to Kuku Kube

--- src/components/kuku-kube/GameArea.tsx
+++ src/components/kuku-kube/GameArea.tsx
@@ -3,23 +3,30 @@ import { generateBoard } from "../../shared/utils/generate-board"
 
 const GameArea = () => {
   const [levelData, setLevelData] = useState(() => generateBoard(6))
-    const submitAnswer = (color: string) => {
-      if(color === levelData.diffColor){
-        setLevelData(generateBoard(6))
-      }
-      return
+  const [score, setScore] = useState(0)
+  const submitAnswer = (color: string) => {
+    if (color === levelData.diffColor) {
+      setScore(prev => prev + 1)
+      setLevelData(generateBoard(6))
     }
+    return
+  }
 
   return (
-    <div className="grid grid-cols-6 gap-4">
-      {levelData.board.map((item, index) => (
-        <div
-          key={index}
-          onClick={() => submitAnswer(item)}
-          className="w-8 h-8"
-          style={{ background: item }}
-        ></div>
-      ))}
+    <div className="flex flex-col gap-4 items-center">
+      <div className="text-3xl font-medium">
+        <span>Счет:</span> <span>{score}</span>
+      </div>
+      <div className="grid grid-cols-6 gap-1">
+        {levelData.board.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => submitAnswer(item)}
+            className="w-8 h-8"
+            style={{ background: item }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
