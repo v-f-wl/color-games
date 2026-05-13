1f86b76531f0940ed4811ec158bafbbf64a61497
Kim Valentin <co.unicorno@mail.ru>
2026-05-14 00:24:45 +0300

    feat: add result screen to kuku kube

---
+++ src/components/kuku-kube/ResultArea.tsx
@@ -0,0 +1,46 @@
+import { Link } from "@tanstack/react-router"
+
+interface ResultAreaProps {
+  score: number
+  difficult: string
+  mode: string
+  onStartAgain: () => void
+}
+const ResultArea = ({
+  score, difficult, mode, onStartAgain
+}: ResultAreaProps) => {
+  return (
+    <section className="w-screen h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-4 items-center">
+        <h2 className="text-4xl font-semibold">
+          Ваш результат:
+        </h2>
+        <span className="text-3xl font-medium text-neutral-600">{score}</span>
+        <div className=""><span>Сложность: {difficult}</span></div>
+        <div className=""><span>Режим: {mode}</span></div>
+
+        <div className="mt-4 flex items-center gap-5">
+          <Link
+            to="/"
+          >
+            На главную
+          </Link>
+          <Link
+            to="/description/$gameId"
+            params={{ gameId: 'kuku-kube' }}
+          >
+            Сменить режим
+          </Link>
+          <div
+            onClick={onStartAgain}
+            className="cursor-pointer"
+          >
+            Начать заново
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ResultArea;
--- src/routes/kuku-kube/index.tsx
+++ src/routes/kuku-kube/index.tsx
@@ -2,24 +2,48 @@ import { useLocation } from "@tanstack/react-router";
 import Header from "../../components/header/Header";
 import GameArea from "../../components/kuku-kube/GameArea";
 import GameNotFound from "../../components/errors/GameNotFound";
+import ResultArea from "../../components/kuku-kube/ResultArea";
+import { useState } from "react";
+
+type KUKUKUBE_STEPS = 'game' | 'result'
 
 export function KukuKube() {
+  const [score, setScore] = useState(0)
+  const [step, setStep] = useState<KUKUKUBE_STEPS>('game')
   const location = useLocation()
   const difficultValue = location.state.difficultValue
   const modeValue = location.state.modeValue
 
-  if(!difficultValue){
-    return <GameNotFound/>
+  if (!difficultValue || !modeValue) {
+    return <GameNotFound />
   }
-    
+
+  const handleStartGameAgain = () => {
+    setScore(() => 0)
+    setStep('game')
+  }
+
   return (
     <>
-      <Header/>
+      <Header />
       <section className="w-full h-screen flex items-center justify-center">
-        <GameArea
-          // difficultValue={difficultValue}
-          difficultValue={difficultValue}
-        />
+        {step === 'game' &&
+          <GameArea
+            difficultValue={difficultValue}
+            onScoreIncrease={() => setScore(prev => prev + 1)}
+            handleFinishGame={() => setStep(() => 'result')}
+            score={score}
+
+          />
+        }
+        {step == 'result' &&
+          <ResultArea
+            score={score}
+            difficult={difficultValue}
+            mode={modeValue}
+            onStartAgain={handleStartGameAgain}
+          />
+        }
       </section>
     </>
   )
