6e0c9bc64cceb79c82e30ba02b249592a6882166
Kim Valentin <co.unicorno@mail.ru>
2026-05-19 23:55:58 +0300

    feat: redesign homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,20 +6,17 @@ const IndexComponent = () => {
   return ( 
     <>
       <Header/>
-      <section className="wrapper max-w-6xl text-black dark:text-white">
-        <h1 className="lg:pt-24 text-5xl text-center font-medium">Проверь своё чувство цвета</h1>
-        <p className="mx-auto font-light text-center mt-5 max-w-2/3">Это игра, в которой ты тренируешь восприятие оттенков и внимание к деталям. Подбирай цвета, находи отличия и проверяй, насколько точно ты видишь разницу. Выбирай режим, повышай сложность и улучшай свой результат с каждой попыткой.</p>
-
-        <h2 className="mt-16 text-center">Выберете режим:</h2>
-        <div className="my-8 flex justify-center gap-8">
-          {GAME_MODES.map(mode => (
-            <GameCard 
-              key={mode.id} 
-              title={mode.title} 
-              description={mode.description} 
-              gameId={mode.id}
-            />
-          ))}
+      <section className="wrapper w-screen h-screen flex flex-col items-centerce justify-center gap-11  text-black dark:text-white">
+          <h1 className="text-5xl text-center font-medium">Проверь своё чувство цвета</h1>
+          <div className="my-8 flex justify-center gap-8">
+            {GAME_MODES.map(mode => (
+              <GameCard 
+                key={mode.id} 
+                title={mode.title} 
+                description={mode.description} 
+                gameId={mode.id}
+              />
+            ))}
         </div>
       </section>
     </>
